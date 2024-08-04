7c921ad006a68e2c8c0b66cd19f992775b679978
Fab2102 <167251292+Fab2102@users.noreply.github.com>
2024-08-04 18:08:29 +0200

    Add files via upload

--- pages/7_🧮_Mischungsrechnung.py
+++ pages/7_🧮_Mischungsrechnung.py
@@ -4,15 +4,17 @@ from oenologics_functions import mixing_liquids
 st.info("Endkonzentration berechnen", icon="ℹ️")
 st.header("Mischungsrechnung")
 
-row1_col1, row1_col2, row1_col3 = st.columns(3)
+row1_col1, row1_col2 = st.columns(2)
 liquid1 = row1_col1.number_input("Flüssigkeit 1 (in Liter)", min_value=0, max_value=200000)
 conc1 = row1_col2.number_input("Konzentration 1", min_value=0.00, max_value=10000.00)
-unit1 = row1_col3.selectbox("Einheit", ("g/l", "mg/l", "°KMW", "vol.%"), key = "unit1")
 
-row2_col1, row2_col2, row2_col3 = st.columns(3)
+row2_col1, row2_col2 = st.columns(2)
 liquid2 = row2_col1.number_input("Flüssigkeit 2 (in Liter)", min_value=0, max_value=200000)
 conc2 = row2_col2.number_input("Konzentration 2", min_value=0.00, max_value=10000.00)
-unit2 = row2_col3.selectbox("Einheit", ("g/l", "mg/l", "°KMW", "vol.%"), key = "unit2")
+
+
+unit = st.selectbox("Einheit", ("g/l", "mg/l", "°KMW", "vol.%"))
+
 
 results_mixing_liquids = mixing_liquids(liquid1, liquid2, conc1, conc2)
 total_volume_l = results_mixing_liquids[0]
@@ -25,4 +27,4 @@ if liquid1 and conc1 and liquid2 and conc2 and total_volume_l and total_concentr
     results_col1, results_col2 = st.columns(2)
 
     results_col1.metric(label="Gesamtmenge (in Liter)", value=f"{total_volume_l:.0f}")
-    results_col2.metric(label=f"Gesamtkonzentration (in {unit1})", value=f"{total_concentration:.2f}")
+    results_col2.metric(label=f"Gesamtkonzentration (in {unit})", value=f"{total_concentration:.2f}")
