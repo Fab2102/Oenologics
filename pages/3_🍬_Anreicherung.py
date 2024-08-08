6114be9fca38ce2d68d09c5a2b677ba802f99fae
Fab2102 <167251292+Fab2102@users.noreply.github.com>
2024-08-08 21:10:09 +0200

    Add files via upload

--- pages/3_🍬_Anreicherung.py
+++ pages/3_🍬_Anreicherung.py
@@ -8,8 +8,13 @@ liter_of_wine = st.number_input("Weinmenge (in Liter)", min_value=0, max_value=2
 additonal_vol = st.number_input("Anreicherung (in vol.%)", min_value=0.00, max_value=3.00)
 sugar_quantity_in_g = add_sugar(liter_of_wine, additonal_vol)
 sugar_quantity_in_kg = sugar_quantity_in_g / 1000
+volume_increase = sugar_quantity_in_kg * 0.6
 
 st.write("######")
 
-if sugar_quantity_in_kg > 0:
-    st.metric(label="Zucker (in kg)", value=f"{sugar_quantity_in_kg:.1f}")
+
+if sugar_quantity_in_kg and volume_increase > 0:
+    result_sugar1, result_sugar2 = st.columns(2)
+    result_sugar1.metric(label="Zucker (in kg)", value=f"{sugar_quantity_in_kg:.1f}")
+    result_sugar2.metric(label="Volumenzunahme (in Liter)", value=f"{volume_increase:.1f}")
+    
